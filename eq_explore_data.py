a6f13dc5ed6fc0010e186643803be9fda3f71238
Pirzmag <pizmak35@gmail.com>
2021-07-06 06:26:40 +0200

    Working with JSONs. Mapping global earthquakes.

---
+++ eq_explore_data.py
@@ -0,0 +1,19 @@
+import json
+
+# Explore the structure of the data
+filename = 'data/eq_data_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+# Get data for all the earthquakes as a list of dicts
+all_eq_dicts = all_eq_data['features']
+
+# Get the magnitudes
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
